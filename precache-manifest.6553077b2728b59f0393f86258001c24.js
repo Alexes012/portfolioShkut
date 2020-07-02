cab94ec8d78f2a552f27d4791a5163931eb7419e
Alexei <61819719+Alexes012@users.noreply.github.com>
2020-07-02 22:45:07 +0300

    Updates

---
+++ precache-manifest.6553077b2728b59f0393f86258001c24.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644f4ded49a542d0d139d206feefea77",
+    "url": "/portfolioShkut/index.html"
+  },
+  {
+    "revision": "b078ea3bb38629850f99",
+    "url": "/portfolioShkut/static/css/main.c4543a42.chunk.css"
+  },
+  {
+    "revision": "34cb4710e3e589473409",
+    "url": "/portfolioShkut/static/js/2.eeab745c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolioShkut/static/js/2.eeab745c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b078ea3bb38629850f99",
+    "url": "/portfolioShkut/static/js/main.ef704a97.chunk.js"
+  },
+  {
+    "revision": "e80ab973d74d0c632779",
+    "url": "/portfolioShkut/static/js/runtime-main.ec755535.js"
+  },
+  {
+    "revision": "c4ad0d80d8260096ac0dd52be0ca1eb3",
+    "url": "/portfolioShkut/static/media/address.c4ad0d80.svg"
+  },
+  {
+    "revision": "4c810933a182adfab2aeaee4b80a156e",
+    "url": "/portfolioShkut/static/media/calculator.4c810933.png"
+  },
+  {
+    "revision": "faecc7e9ce4b092101abf574ea27fd53",
+    "url": "/portfolioShkut/static/media/counterFull.faecc7e9.jpg"
+  },
+  {
+    "revision": "ceac94df29817c4438dab93e6f165dd6",
+    "url": "/portfolioShkut/static/media/css.ceac94df.svg"
+  },
+  {
+    "revision": "e32b2ff807c8dc41760bb012ef53e415",
+    "url": "/portfolioShkut/static/media/github.e32b2ff8.svg"
+  },
+  {
+    "revision": "1b4a6dbc03f9f5cdec8fdca32a565376",
+    "url": "/portfolioShkut/static/media/html-5.1b4a6dbc.svg"
+  },
+  {
+    "revision": "246382bdf97c0b4b4284773973d88591",
+    "url": "/portfolioShkut/static/media/javascript.246382bd.svg"
+  },
+  {
+    "revision": "244e0ae29f723a7ac3fe38082a9b3e24",
+    "url": "/portfolioShkut/static/media/mail.244e0ae2.svg"
+  },
+  {
+    "revision": "8ee5fa2e7dd1d598cd7a9dec325de48d",
+    "url": "/portfolioShkut/static/media/myphoto.8ee5fa2e.png"
+  },
+  {
+    "revision": "6aed710079c9379571bd7b13b3af9a30",
+    "url": "/portfolioShkut/static/media/noProject.6aed7100.png"
+  },
+  {
+    "revision": "9ec95192b5679587d8b130fea5161e84",
+    "url": "/portfolioShkut/static/media/phone.9ec95192.svg"
+  },
+  {
+    "revision": "18e7877a5c54aee13b583276739f192e",
+    "url": "/portfolioShkut/static/media/react.18e7877a.svg"
+  },
+  {
+    "revision": "52dc1b5671efa866b10bf9a72678c89c",
+    "url": "/portfolioShkut/static/media/redux.52dc1b56.svg"
+  },
+  {
+    "revision": "5e9cb11ddb18a5370851ceb4401b9686",
+    "url": "/portfolioShkut/static/media/typescript.5e9cb11d.svg"
+  }
+]);
